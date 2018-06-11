86724c7088d6c4c6fbd327a001adcf36ea248859
Marius Lindvall <marius@varden.info>
2018-06-11 20:11:58 +0200

    Make config into class

--- includes/lib/config.php
+++ includes/lib/config.php
@@ -1,27 +1,29 @@
 <?php
 
-function getConfig($path) {
-    $configLocation = __DIR__."/../config.json";
-    
-    if (!file_exists($configLocation)) return getDefaultConfig($path);
-    $config = json_decode(file_get_contents($configLocation), true);
-    
-    $segments = explode("/", $path);
-    foreach ($segments as $segment) {
-        if (!isset($config[$segment])) return getDefaultConfig($path);
-        $config = $config[$segment];
+class Config {
+    public static function get($path) {
+        $configLocation = __DIR__."/../config.json";
+        
+        if (!file_exists($configLocation)) return getDefaultConfig($path);
+        $config = json_decode(file_get_contents($configLocation), true);
+        
+        $segments = explode("/", $path);
+        foreach ($segments as $segment) {
+            if (!isset($config[$segment])) return getDefaultConfig($path);
+            $config = $config[$segment];
+        }
+        
+        return $config;
     }
-    
-    return $config;
-}
 
-function getDefaultConfig($path) {
-    $defaults = array(
-        "access/require-login" => false
-    );
-    
-    if (isset($defaults[$path])) return $defaults[$path];
-    return null;
+    public static function getDefault($path) {
+        $defaults = array(
+            "access/require-login" => false
+        );
+        
+        if (isset($defaults[$path])) return $defaults[$path];
+        return null;
+    }
 }
 
 ?>
