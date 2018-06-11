98c7339ce2fccc0ec62e08fdbeeb024705887cba
Marius Lindvall <marius@varden.info>
2018-06-11 12:48:39 +0200

    Change defaults handling

--- includes/lib/config.php
+++ includes/lib/config.php
@@ -1,18 +1,24 @@
 <?php
 
-function getConfig($path, $default = null) {
+function getConfig($path) {
     $configLocation = __DIR__."/../config.json";
     
-    if (!file_exists($configLocation)) return $default;
+    if (!file_exists($configLocation)) return getDefaultConfig($path);
     $config = json_decode(file_get_contents($configLocation), true);
     
     $segments = explode("/", $path);
     foreach ($segments as $segment) {
-        if (!isset($config[$segment])) return $default;
+        if (!isset($config[$segment])) return getDefaultConfig($path);
         $config = $config[$segment];
     }
     
     return $config;
+}
+
+function getDefaultConfig($path) {
+    $defaults = array(
+        "access/require-login" => false
+    );
 }
 
 ?>
