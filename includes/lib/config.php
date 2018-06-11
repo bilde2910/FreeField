9a760868999c4cf3fcb933b6dc44b525dbeb297d
Marius Lindvall <marius@varden.info>
2018-06-11 20:19:12 +0200

    Fix broken PHP

--- includes/lib/config.php
+++ includes/lib/config.php
@@ -1,28 +1,38 @@
 <?php
 
 class Config {
+    private static $config = false;
+    
     public static function get($path) {
-        $configLocation = __DIR__."/../config.json";
+        if (self::$config === false) self::loadConfig();
         
-        if (!file_exists($configLocation)) return getDefaultConfig($path);
-        $config = json_decode(file_get_contents($configLocation), true);
+        $conf = self::config;
         
         $segments = explode("/", $path);
         foreach ($segments as $segment) {
-            if (!isset($config[$segment])) return getDefaultConfig($path);
-            $config = $config[$segment];
+            if (!isset($conf[$segment])) return self::getDefaultConfig($path);
+            $conf = $conf[$segment];
         }
         
-        return $config;
+        return $conf;
     }
 
     public static function getDefault($path) {
         $defaults = array(
-            "access/require-login" => false
+            "access/require-login" => false,
+            "security/validate-lang" => true,
+            "security/validate-ua" => true
         );
         
         if (isset($defaults[$path])) return $defaults[$path];
         return null;
+    }
+    
+    private static function loadConfig() {
+        $configLocation = __DIR__."/../config.json";
+        
+        if (!file_exists($configLocation)) return self::getDefaultConfig($path);
+        self::$config = json_decode(file_get_contents($configLocation), true);
     }
 }
 
