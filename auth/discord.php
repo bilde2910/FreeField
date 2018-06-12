c93f5d087c9e6c01c3ebd6837afcbb9e20160661
Marius Lindvall <marius@varden.info>
2018-06-12 13:32:31 +0200

    Implemented Discord authentication

---
+++ auth/discord.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once("./includes/lib/global.php");
+__require("config");
+
+$service = "discord";
+
+$require_config = array(
+    "auth/provider/{$service}/client-id",
+    "auth/provider/{$service}/client-secret"
+);
+if (Config::ifAny($require_config, null)) {
+    header("HTTP/1.1 307 Temporary Redirect");
+    header("Location: ".Config::getEndpointUri("/auth/login.php"));
+    exit;
+}
+
+__require("vendor");
+
+$provider = new \Wohali\OAuth2\Client\Provider\Discord([
+    "clientId" => Config::get("auth/provider/{$service}/client-id"),
+    "clientSecret" => Config::get("auth/provider/{$service}/client-secret"),
+    "redirectUri" => Config::getEndpointUri("/auth/{$service}.php")
+]);
+
+if (!isset($_GET["code"])) {
+    $authUrl = $provider->getAuthorizationUrl(array(
+        "state" => "OPTIONAL_CUSTOM_CONFIGURED_STATE",
+        "scope" => array("identify")
+    ));
+    header("HTTP/1.1 307 Temporary Redirect");
+    setcookie("oa2-{$service}-state", $provider->getState(), 0, $_SERVER["REQUEST_URI"]);
+    header("Location: {$authUrl}");
+    exit;
+} elseif (empty($_GET["state"]) || !isset($_COOKIE["oa2-{$service}-state"]) || $_GET["state"] !== $_COOKIE["oa2-{$service}-state"]) {
+    header("303 See Other");
+    setcookie("oa2-{$service}-state", "", time() - 3600, $_SERVER["REQUEST_URI"]);
+    header("Location: ".Config::getEndpointUri("/auth/failed.php?provider={$service}"));
+    exit;
+} else {
+    $token = $provider->getAccessToken("authorization_code", array(
+        "code" => $_GET["code"]
+    ));
+    try {
+        $user = $provider->getResourceOwner($token);
+        
+        __require("auth");
+        Auth::setAuthenticatedSession($user->getId(), Config::get("auth/session-length"));
+        header("HTTP/1.1 303 See Other")
+        header("Location: ".Config::getEndpointUri("/"));
+        
+    } catch (Exception $e) {
+        header("303 See Other");
+        setcookie("oa2-{$service}-state", "", time() - 3600, $_SERVER["REQUEST_URI"]);
+        header("Location: ".Config::getEndpointUri("/auth/failed.php?provider={$service}"));
+        exit;
+    }
+}
+
+?>
--- includes/lib/config.php
+++ includes/lib/config.php
@@ -16,10 +16,19 @@ class Config {
         
         return $conf;
     }
+    
+    public static function ifAny($paths, $value) {
+        foreach ($paths as $path) {
+            if (self::get($path) === $value) return true;
+        }
+        return false;
+    }
 
     public static function getDefault($path) {
         $defaults = array(
-            "access/require-login" => false,
+            "auth/provider/discord/client-id" => null,
+            "auth/provider/discord/client-secret" => null,
+            "auth/session-length" => 315576000, // 10 years of 365.25 days
             "database/host" => "localhost",
             "database/database" => "fieldfree",
             "database/password" => "fieldfree",
@@ -27,12 +36,20 @@ class Config {
             "database/table-prefix" => "ffield_",
             "database/type" => "mysqli",
             "database/username" => "fieldfree",
+            "permissions/level/access" => 0,
             "security/validate-lang" => true,
-            "security/validate-ua" => true
+            "security/validate-ua" => true,
+            "setup/uri" => null,
+            "user/require-validation" => false
         );
         
         if (isset($defaults[$path])) return $defaults[$path];
         return null;
+    }
+    
+    public static function getEndpointUri($endpoint) {
+        $basepath = self::get("setup/uri");
+        return (substr($basepath, 0, 1) == "/" ? substr($basepath, 1) : $basepath).$endpoint;
     }
     
     private static function loadConfig() {
