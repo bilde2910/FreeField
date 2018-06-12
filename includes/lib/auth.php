e1c949476d2db384a01b9343a9b4208cdd22352c
Marius Lindvall <marius@varden.info>
2018-06-12 13:14:04 +0200

    Compartmentalized user data structures for OOP principles and improved reusability

--- includes/lib/auth.php
+++ includes/lib/auth.php
@@ -7,60 +7,12 @@ require_once(__DIR__."/../sessionkey.php");
 
 class Auth {
     // Cache for the current authenticated user to prevent unnecessary database lookups.
-    private static $authSessionCache = false;
+    private static $authSessionCache = null;
     
     // Gets the User-Agent without version numbers for identifying a specific browser type.
     private static function getUnversionedUserAgent() {
         if (!isset($_SERVER["HTTP_USER_AGENT"])) return "";
         return preg_replace('@/[^ ]+@', "", $_SERVER["HTTP_USER_AGENT"]);
-    }
-
-    // Gets the nickname of the current user.
-    public static function getNickname() {
-        $session = self::getAuthenticatedSession();
-        if ($session === null) return null;
-        return $session["nick"];
-    }
-    
-    // Gets the current user ID.
-    public static function getUserID() {
-        $session = self::getAuthenticatedSession();
-        if ($session === null) return null;
-        return $session["id"];
-    }
-    
-    // Gets the current user permission level.
-    public static function getPermissionLevel() {
-        $session = self::getAuthenticatedSession();
-        if ($session === null) return 0;
-        return $session["level"];
-    }
-    
-    // Checks whether the user has the given permission.
-    public static function hasPermission($permission) {
-        $session = self::getAuthenticatedSession();
-        if ($session !== null) {
-            $explperms = explode(",", $session["overrides"]);
-            foreach ($explperms as $perm) {
-                if (substr($perm, 1) == $permission) {
-                    if (substr($perm, 0, 1) == "+") return true;
-                    if (substr($perm, 0, 1) == "-") return false;
-                }
-            }
-        }
-        $perm = Config::get("permissions/level/{$permission}");
-        return ($session === null ? 0 : $session["level"]) >= $perm;
-    }
-    
-    // Checks whether the user has a user-level override for the given permission.
-    public static function hasExplicitRights($permission) {
-        $session = self::getAuthenticatedSession();
-        if ($session === null) return false;
-        $explperms = explode(",", $session["overrides"]);
-        foreach ($explperms as $perm) {
-            if (substr($perm, 1) == $permission) return true;
-        }
-        return false;
     }
 
     // Gets the raw, unauthenticated session array from cookie data.
@@ -117,14 +69,25 @@ class Auth {
         $session = base64_encode($iv.$hmac.$ciph);
         setcookie("session", $session, time() + $expire);
     }
+    
+    // Get user by ID.
+    public static function getUser($id) {
+        $db = Database::getSparrow();
+        $userdata = $db
+            ->from(Database::getTable("users"))
+            ->where("id", $session["id"])
+            ->one();
+        
+        return new User($userdata);
+    }
 
     // Authenticates the current cookie session data against the user database.
     private static function getAuthenticatedSession() {
-        if (self::$authSessionCache !== false) return self::$authSessionCache;
+        if (self::$authSessionCache !== null) return self::$authSessionCache;
         
         $session = self::getSession();
-        if ($session === null) return null;
-        if ($session["expire"] < time()) return null;
+        if ($session === null) return self::setReturnUser(new User(null));
+        if ($session["expire"] < time()) return self::setReturnUser(new User(null));
         
         $selectors = array();
         
@@ -132,23 +95,84 @@ class Auth {
         if (Config::get("security/validate-lang")) $selectors["http-lang"] = isset($_SERVER["HTTP_ACCEPT_LANGUAGE"]) ? $_SERVER["HTTP_ACCEPT_LANGUAGE"] : "";
         
         foreach ($selectors as $selectors => $expectedValue) {
-            if ($session[$selector] != $expectedValue) return null;
+            if ($session[$selector] != $expectedValue) return self::setReturnUser(new User(null));
         }
         
         $db = Database::getSparrow();
-        $user = $db
+        $userdata = $db
             ->from(Database::getTable("users"))
             ->where("id", $session["id"])
             ->where("token", $session["token"])
             ->one();
         
+        return self::setReturnUser(new User($userdata));
+    }
+    
+    // Sets the current user to the given user and returns it.
+    private static function setReturnUser($user) {
         self::$authSessionCache = $user;
         return $user;
     }
     
     // Returns whether or not the user is authenticated
     public static function isAuthenticated() {
-        return getAuthenticatedSession() !== null;
+        return self::getAuthenticatedSession()->exists();
+    }
+}
+
+class User {
+    private $data = null;
+    
+    function __construct($userdata) {
+        $this->data = $userdata;
+    }
+    
+    // Gets whether or not the user exists. If false, the user is not logged in.
+    public function exists() {
+        return $this->data !== null && count($this->data > 0);
+    }
+    
+    // Gets the nickname of the current user.
+    public function getNickname() {
+        if (!$this->exists()) return null;
+        return $this->data["nick"];
+    }
+    
+    // Gets the current user ID.
+    public function getUserID() {
+        if (!$this->exists()) return null;
+        return $this->data["id"];
+    }
+    
+    // Gets the current user permission level.
+    public function getPermissionLevel() {
+        if (!$this->exists()) return 0;
+        return $this->data["level"];
+    }
+    
+    // Checks whether the user has the given permission.
+    public function hasPermission($permission) {
+        if (!$this->exists()) {
+            $explperms = explode(",", $this->data["overrides"]);
+            foreach ($explperms as $perm) {
+                if (substr($perm, 1) == $permission) {
+                    if (substr($perm, 0, 1) == "+") return true;
+                    if (substr($perm, 0, 1) == "-") return false;
+                }
+            }
+        }
+        $perm = Config::get("permissions/level/{$permission}");
+        return ($this->data === null ? 0 : $this->data["level"]) >= $perm;
+    }
+    
+    // Checks whether the user has a user-level override for the given permission.
+    public function hasExplicitRights($permission) {
+        if (!$this->exists()) return false;
+        $explperms = explode(",", $this->data["overrides"]);
+        foreach ($explperms as $perm) {
+            if (substr($perm, 1) == $permission) return true;
+        }
+        return false;
     }
 }
 
