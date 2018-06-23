764b357039b2ce0dc4206718bb2e7e3c37a0b5a1
Marius Lindvall <marius@varden.info>
2018-06-23 21:08:50 +0200

    Change login color theme to user theme

--- auth/login.php
+++ auth/login.php
@@ -41,7 +41,7 @@ $providerAppearance = array(
         <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/main.css">
-        <link rel="stylesheet" href="../css/<?php echo Config::get("themes/color/admin"); ?>.css">
+        <link rel="stylesheet" href="../css/<?php echo Config::get("themes/color/user-settings/theme"); ?>.css">
         
         <!--[if lte IE 8]>
             <link rel="stylesheet" href="../css/layouts/side-menu-old-ie.css">
