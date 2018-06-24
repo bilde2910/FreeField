c7b78ed4e45bfdceacb56a4869dc07943d4a76b6
Marius Lindvall <marius@varden.info>
2018-06-24 14:12:39 +0200

    Fix endpoint URI call settings

--- includes/lib/config.php
+++ includes/lib/config.php
@@ -395,7 +395,7 @@ class Config {
     }
     
     public static function getEndpointUri($endpoint) {
-        $basepath = self::get("setup/uri");
+        $basepath = self::get("site/uri");
         return (substr($basepath, -1) == "/" ? substr($basepath, 0, -1) : $basepath).$endpoint;
     }
     
