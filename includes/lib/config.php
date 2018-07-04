62c20aff1c31bf355245e1079d7caff5d521b397
Marius Lindvall <marius@varden.info>
2018-07-04 16:56:34 +0200

    Newline indentation consistency

--- includes/lib/config.php
+++ includes/lib/config.php
@@ -2,23 +2,23 @@
 
 class Config {
     private const CONFIG_LOCATION = __DIR__."/../config.json";
-    
+
     private static $config = false;
     private static $flattree = null;
-    
+
     /*
         Configtree is a tree consisting of all config options and their possible values.
         The setup of the tree is of the form DOMAIN -> SECTION -> SETTING. Domain is the
         page on the administration interface the setting should show up on. Section is
         the section on that page where the settings would appear. Each setting is then
         listed in order of appearance. Each setting key must be globally unique.
-        
+
         E.g. "setup/uri" is listed under the Access section on the main settings page.
-        
+
         Each setting has two options:
         - default is the default value of the object
         - options specifies the type of data to store.
-        
+
         Valid options:
         - "string" for a string
         - "password" for a password-type string
@@ -29,7 +29,7 @@ class Config {
         - "permission" for a permission tier
         - "bool" for a boolean
         - array() for a selection box with the array contents as options
-        
+
         I18N is handled with setting.<setting>.name and setting.<setting>.desc.
         For domains and sections, it's handled with admin.domain.<domain>.name and
         admin.domain.<domain>.desc, and admin.section.<domain>.<section>.name and
@@ -37,35 +37,35 @@ class Config {
         description if __hasdesc is set. If __hasdesc is not true,
         admin.section.<domain>.<section>.desc is ignored. Section descriptions can
         be formatted similar to sprintf() with __descsprintf (optional). Example:
-        
+
         Example I18N'd string: "Please set up {%1} authentication first!"
         If __descsprintf = array("Discord"); the string would be:
         "Please set up Discord authentication first!"
-        
+
         Selection box contents are I18N'd with setting.<setting>.option.<option>.
         Dashes in all parts of the I18N string are replaced with underscores and
         slashes replaced with dots.
-        
+
         E.g. for setting "setup/uri":
         - setting.setup.uri.name
         - setting.setup.uri.desc
-        
+
         E.g. for setting "security/validate-ua":
         - setting.security.validate_ua.name
         - setting.security.validate_ua.desc
         - setting.security.validate_ua.option.no
         - setting.security.validate_ua.option.lenient
         - setting.security.validate_ua.option.strict
-        
+
         E.g. for domain "main":
         - admin.domain.main.name
         - admin.domain.main.desc
-        
+
         E.g. for section "database" in domain "main":
         - admin.section.main.database.name
         - admin.section.main.database.desc
     */
-    
+
     private static $configtree = array(
         "main" => array(
             "access" => array(
@@ -241,12 +241,12 @@ class Config {
             )
         )
     );
-    
+
     public static function get($path) {
         if (self::$config === false) self::loadConfig();
-        
+
         $conf = self::$config;
-        
+
         $segments = explode("/", $path);
         foreach ($segments as $segment) {
             if (!isset($conf[$segment])) return self::getDefault($path);
@@ -254,17 +254,17 @@ class Config {
         }
         return $conf;
     }
-    
+
     public static function set($options) {
         if (self::$config === false) self::loadConfig();
         $flat = self::getFlatTree();
-        
+
         foreach ($options as $option => $value_raw) {
             if (!isset($flat[$option])) continue;
             $values = $flat[$option];
-            
+
             $value = null;
-            
+
             if (is_array($values["options"])) {
                 $type = gettype($values["options"][0]);
                 switch ($type) {
@@ -306,7 +306,7 @@ class Config {
                         break;
                 }
             }
-            
+
             $s = explode("/", $option);
             switch (count($s)) {
                 case 1:
@@ -343,18 +343,18 @@ class Config {
         }
         self::saveConfig();
     }
-    
+
     public static function ifAny($paths, $value) {
         foreach ($paths as $path) {
             if (self::get($path) === $value) return true;
         }
         return false;
     }
-    
+
     private static function getFlatTree() {
         if (self::$flattree !== null) return self::$flattree;
         self::$flattree = array();
-        
+
         foreach (self::$configtree as $domain => $sections) {
             foreach ($sections as $section => $paths) {
                 foreach ($paths as $path => $values) {
@@ -362,10 +362,10 @@ class Config {
                 }
             }
         }
-        
+
         return self::$flattree;
     }
-    
+
     public static function getTreeDomain($domain) {
         return self::$configtree[$domain];
     }
@@ -377,28 +377,28 @@ class Config {
         }
         return null;
     }
-    
+
     public static function getSettingI18N($path) {
         return new ConfigSettingI18N($path, self::getFlatTree()[$path]);
     }
-    
+
     public static function getSectionI18N($domain, $section) {
         return new ConfigSectionI18N($domain, $section);
     }
-    
+
     public static function getDomainI18N($domain) {
         return new ConfigDomainI18N($domain);
     }
-    
+
     public static function translatePathI18N($path) {
         return str_replace("/", ".", str_replace("-", "_", $path));
     }
-    
+
     public static function getEndpointUri($endpoint) {
         $basepath = self::get("site/uri");
         return (substr($basepath, -1) == "/" ? substr($basepath, 0, -1) : $basepath).$endpoint;
     }
-    
+
     private static function loadConfig() {
         if (!file_exists(self::CONFIG_LOCATION)) {
             self::$config = array();
@@ -406,7 +406,7 @@ class Config {
             self::$config = json_decode(file_get_contents(self::CONFIG_LOCATION), true);
         }
     }
-    
+
     private static function saveConfig() {
         file_put_contents(self::CONFIG_LOCATION, json_encode(self::$config, JSON_PRETTY_PRINT));
     }
@@ -415,24 +415,24 @@ class Config {
 class ConfigSettingI18N {
     private $path = null;
     private $setting = null;
-    
+
     function __construct($path, $setting) {
         $this->path = $path;
         $this->setting = $setting;
     }
-    
+
     public function getName() {
         return "setting.".Config::translatePathI18N($this->path).".name";
     }
-    
+
     public function getDescription() {
         return "setting.".Config::translatePathI18N($this->path).".desc";
     }
-    
+
     public function getOption($option) {
         return "setting.".Config::translatePathI18N($this->path).".option.".Config::translatePathI18N($option);
     }
-    
+
     public function getLabel() {
         return "setting.".Config::translatePathI18N($this->path).".label";
     }
@@ -441,16 +441,16 @@ class ConfigSettingI18N {
 class ConfigSectionI18N {
     private $domain = null;
     private $section = null;
-    
+
     function __construct($domain, $section) {
         $this->domain = $domain;
         $this->section = $section;
     }
-    
+
     public function getName() {
         return "admin.section.".Config::translatePathI18N($this->domain).".".Config::translatePathI18N($this->section).".name";
     }
-    
+
     public function getDescription() {
         return "admin.section.".Config::translatePathI18N($this->domain).".".Config::translatePathI18N($this->section).".desc";
     }
@@ -458,19 +458,19 @@ class ConfigSectionI18N {
 
 class ConfigDomainI18N {
     private $domain = null;
-    
+
     function __construct($domain) {
         $this->domain = $domain;
     }
-    
+
     public function getName() {
         return "admin.domain.".Config::translatePathI18N($this->domain).".name";
     }
-    
+
     public function getDescription() {
         return "admin.domain.".Config::translatePathI18N($this->domain).".desc";
     }
-    
+
     public function getSection($section) {
         return new ConfigSectionI18N($this->domain, $section);
     }
