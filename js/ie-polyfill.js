9f0d5466781d7c2308b8da1d9994be946354b3cf
Marius Lindvall <marius@varden.info>
2018-11-17 16:32:31 +0100

    Add String.prototype.endsWith() polyfill

--- js/ie-polyfill.js
+++ js/ie-polyfill.js
@@ -2,8 +2,26 @@
     This script file is a polyfill for Internet Explorer, which does not support
     ECMAScript 6.
 */
+
+/*
+    String.prototype.startsWith() polyfill
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith#Polyfill
+*/
 if (!String.prototype.startsWith) {
 	String.prototype.startsWith = function(search, pos) {
 		return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search;
+	};
+}
+
+/*
+    String.prototype.endsWith() polyfill
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith#Polyfill
+*/
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
 	};
 }
