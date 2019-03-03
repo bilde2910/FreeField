b0c2c3fde4838458db4efa93b32e4c60cb2e8cfe
Marius Lindvall <marius@varden.info>
2019-03-03 22:01:37 +0100

    Increment version number pending release

--- includes/lib/global.php
+++ includes/lib/global.php
@@ -10,7 +10,7 @@
     then call the proper module using `__require()`.
 */
 
-const FF_VERSION = "1.1-beta.1";
+const FF_VERSION = "1.1-beta.2";
 
 function __require($require) {
     switch ($require) {
